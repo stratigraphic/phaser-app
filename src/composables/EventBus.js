958253ab7e090e2336510c802938bc0c308f0ca2
Ceri Binding <cbinding@glam.ac.uk>
2022-03-08 11:54:39 +0000

    Delete src directory

--- src/composables/EventBus.js
+++
@@ -1,7 +0,0 @@
-// EventBus used to communicate directly between components 
-// usage: import EventBus from '@/composables/EventBus.js'
-// event sender: EventBus.$emit("my-event-name")
-// event receiver: EventBus.$on("my-event-name", () => doSomething())
-import Vue from 'vue'
-const EventBus = new Vue()
-export default EventBus
